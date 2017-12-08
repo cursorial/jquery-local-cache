bbf785430b7ca223d9222f5c57bda3573c02b976
Michael <mike@vps489630.ovh.net>
2017-12-08 12:28:10 +0100

    first commit

---
+++ localCache.js
@@ -0,0 +1,36 @@
+(function ($) {
+	    var localCache = {
+		            data: {},
+		            get: function(url) {
+				                return localCache.data[url];
+				            },
+		            set: function(url, cachedData, callback) {
+				                localCache.remove(url);
+				                localCache.data[url] = cachedData;
+				                if($.isFunction(callback)) {
+							                callback(cachedData);
+							            }
+				            },
+		            remove: function(url) {
+				                delete localCache.data[url];
+				            },
+		            exist: function(url) {
+				                return localCache.data.hasOwnProperty(url) && localCache.data[url] !== null;
+				            }
+		        };
+
+	    $.fn.localCache = function(action, url, options) {
+		            if(action === 'get') {
+				                localCache.get(url);
+				            }
+		            if(action === 'set') {
+				                localCache.set(url, options.data, options.callback);
+				            }
+		            if(action === 'remove') {
+				                localCache.remove(url);
+				            }
+		            if(action === 'exists') {
+				                localCache.exists(url);
+				            }
+		        }
+})
